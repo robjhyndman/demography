0b9af21b45045aa6780fa048ceaedc13482b46da
robjhyndman <robjhyndman@gmail.com>
2016-12-23 10:08:01 +1100

    Update to HMD url

--- R/hmd.R
+++ R/hmd.R
@@ -1,24 +1,24 @@
 # Function to construct a mortality demogdata object from HMD
 hmd.mx <- function(country, username, password, label=country)
 {
-    path <- paste("http://www.mortality.org/hmd/", country, "/STATS/", "Mx_1x1.txt", sep = "")
+    path <- paste("http://mortality.org/hmd/", country, "/STATS/", "Mx_1x1.txt", sep = "")
     userpwd <- paste(username, ":", password, sep = "")
     txt <- RCurl::getURL(path, userpwd = userpwd)
     con <- textConnection(txt)
     mx <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
     close(con)
     if(class(mx)=="try-error")
-        stop("Connection error at www.mortality.org. Please check username, password and country label.")
+        stop("Connection error at mortality.org. Please check username, password and country label.")
 
-    path <- paste("http://www.mortality.org/hmd/", country, "/STATS/", "Exposures_1x1.txt", sep = "")
+    path <- paste("http://mortality.org/hmd/", country, "/STATS/", "Exposures_1x1.txt", sep = "")
     userpwd <- paste(username, ":", password, sep = "")
     txt <- RCurl::getURL(path, userpwd = userpwd)
     con <- textConnection(txt)
     pop <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
     close(con)
     if(class(pop)=="try-error")
-        stop("Exposures file not found at www.mortality.org")
-        
+        stop("Exposures file not found at mortality.org")
+
     obj <- list(type="mortality",label=label,lambda=0)
 
     obj$year <- sort(unique(mx[, 1]))
@@ -29,7 +29,7 @@ hmd.mx <- function(country, username, password, label=country)
     mnames <- names(mx)[-c(1, 2)]
     n.mort <- length(mnames)
     obj$rate <- obj$pop <- list()
-    for (i in 1:n.mort) 
+    for (i in 1:n.mort)
     {
         obj$rate[[i]] <- matrix(mx[, i + 2], nrow = m, ncol = n)
         obj$rate[[i]][obj$rate[[i]] < 0] <- NA
@@ -40,7 +40,7 @@ hmd.mx <- function(country, username, password, label=country)
     names(obj$pop) = names(obj$rate) <- tolower(mnames)
 
     obj$age <- as.numeric(as.character(obj$age))
-    if (is.na(obj$age[m])) 
+    if (is.na(obj$age[m]))
         obj$age[m] <- 2 * obj$age[m - 1] - obj$age[m - 2]
     return(structure(obj, class = "demogdata"))
 }
@@ -48,14 +48,14 @@ hmd.mx <- function(country, username, password, label=country)
 
 hmd.e0 <- function(country, username, password)
 {
-    path <- paste("http://www.mortality.org/hmd/", country, "/STATS/", "E0per.txt", sep = "")
+    path <- paste("http://mortality.org/hmd/", country, "/STATS/", "E0per.txt", sep = "")
     userpwd <- paste(username, ":", password, sep = "")
     txt <- RCurl::getURL(path, userpwd = userpwd)
     con <- textConnection(txt)
     lt <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."), TRUE)
     close(con)
-    if (class(lt) == "try-error") 
-        stop("Life expectancy file not found at www.mortality.org")
+    if (class(lt) == "try-error")
+        stop("Life expectancy file not found at mortality.org")
 	lt <- ts(lt[,-1],start=lt[1,1],frequency=1)
     return(lt)
 }
@@ -63,15 +63,15 @@ hmd.e0 <- function(country, username, password)
 
 hmd.pop <- function(country, username, password, label=country)
 {
-    path <- paste("http://www.mortality.org/hmd/", country, "/STATS/", "Population.txt", sep = "")
+    path <- paste("http://mortality.org/hmd/", country, "/STATS/", "Population.txt", sep = "")
     userpwd <- paste(username, ":", password, sep = "")
     txt <- RCurl::getURL(path, userpwd = userpwd)
     con <- textConnection(txt)
     pop <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
     close(con)
     if(class(pop)=="try-error")
-        stop("Population file not found at www.mortality.org")
-        
+        stop("Population file not found at mortality.org")
+
     obj <- list(type="population",label=label,lambda=0)
 
     obj$year = sort(unique(pop[, 1]))
@@ -82,7 +82,7 @@ hmd.pop <- function(country, username, password, label=country)
     mnames <- names(pop)[-c(1, 2)]
     n.pop <- length(mnames)
     obj$pop <- list()
-    for (i in 1:n.pop) 
+    for (i in 1:n.pop)
     {
         obj$pop[[i]] <- matrix(pop[, i + 2], nrow = m, ncol = n)
         obj$pop[[i]][obj$pop[[i]] < 0] <- NA
@@ -91,7 +91,7 @@ hmd.pop <- function(country, username, password, label=country)
     names(obj$pop) <- tolower(mnames)
 
     obj$age <- as.numeric(as.character(obj$age))
-    if (is.na(obj$age[m])) 
+    if (is.na(obj$age[m]))
         obj$age[m] <- 2 * obj$age[m - 1] - obj$age[m - 2]
     return(structure(obj, class = "demogdata"))
 }
