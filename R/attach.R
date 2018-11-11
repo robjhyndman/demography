2f69faf536121374a843f890348d7bddccae4ab0
Fin Yang <YangYangzhuoran@gmail.com>
2018-11-12 02:43:53 +1100

    fixed library/load_all error

--- R/attach.R
+++ R/attach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...)
 {
-    library(help=demography)$info[[1]] -> version
+    library(help=demography, lib.loc = .libPaths())$info[[1]] -> version
     version <- version[pmatch("Version",version)]
     um <- strsplit(version," ")[[1]]
     version <- um[nchar(um)>0][2]
