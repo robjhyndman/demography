4c13867faf1567b36c8126e9d10113e032fa90d8
Fin Yang <YangYangzhuoran@gmail.com>
2018-11-15 19:26:14 +1100

    roxygened demogdata.R demography.R

--- R/demography.R
+++ R/demography.R
@@ -1,4 +1,12 @@
-
+#' Forecasting mortality and fertility data
+#' 
+#' Functions for demographic analysis including lifetable calculations,
+#' Lee-Carter modelling and functional data analysis of mortality rates.
+#' 
+#' 
+#' @author Rob J Hyndman with contributions from Heather Booth, Leonie Tickle, John Maindonald, Simon Wood and the R Core Team.  
+#' @author Maintainer: <Rob.Hyndman@monash.edu>
+#' 
 #' @import forecast 
 #' @import ftsa 
 #' @import rainbow 
@@ -9,4 +17,10 @@
 #' @importFrom stats poisson predict qnorm rbinom rpois spline splinefun start time 
 #' @importFrom stats ts tsp tsp<- uniroot window median 
 #' @importFrom utils read.table 
+#' 
+#' @docType package
+#' 
+#' @name demography-package
+#' @aliases demography
+#' @keywords package
 NULL
