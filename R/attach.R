b726d2e5ddca5f79f16cf354180097471b29393a
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-05 10:41:11 +1100

    Fixed issue with using devtools::load_all()

--- R/attach.R
+++ R/attach.R
@@ -1,8 +1,10 @@
 .onAttach <- function(...)
 {
     library(help=demography)$info[[1]] -> version
-    version <- version[pmatch("Version",version)]
-    um <- strsplit(version," ")[[1]]
-    version <- um[nchar(um)>0][2]
-    packageStartupMessage(paste("This is demography",version,"\n"))
+    if (!is.null(version)){
+        version <- version[pmatch("Version",version)]
+        um <- strsplit(version," ")[[1]]
+        version <- um[nchar(um)>0][2]
+        packageStartupMessage(paste("This is demography",version,"\n"))      
+    }
 }
