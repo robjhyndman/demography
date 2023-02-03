3e5d1c70a4d506f8d9dbea66144624792617c752
Rob Hyndman <robjhyndman@gmail.com>
2023-02-03 12:58:09 +1100

    Now using HMDHFDPlus for downloading HMD data

--- R/hmd.R
+++ R/hmd.R
@@ -1,6 +1,4 @@
-# Function to construct a mortality demogdata object from HMD
-
-#' Summary for functional demographic model or Lee-Carter model
+#' Read data from HMD and construct a mortality demogdata object
 #'
 #' \code{hmd.mx} reads "Mx" (1x1) data from the Human Mortality Database (HMD
 #' \url{https://www.mortality.org}) and constructs a demogdata object suitable
@@ -12,67 +10,10 @@
 #' result as a \code{ts} object.
 #'
 #' In order to read the data, users are required to create their account via the HMD website (\url{https://www.mortality.org}),
-#' and obtain a valid username and password.
-#'
-#' The country codes (as at 23 December 2016) are as follows.
-#' \tabular{ll}{
-#'   Australia \tab AUS\cr
-#'   Austria \tab AUT\cr
-#'   Belarus \tab BLR\cr
-#'   Belgium \tab BEL\cr
-#'   Bulgaria \tab BGR\cr
-#'   Canada \tab CAN\cr
-#'   Chile \tab CHL\cr
-#'   Czech Republic \tab CZE\cr
-#'   Denmark \tab DNK\cr
-#'   Estonia \tab EST\cr
-#'   Finland \tab FIN\cr
-#'   France\cr
-#'   -- France total population \tab FRATNP\cr
-#'   -- France civilian population \tab FRACNP\cr
-#'   Germany\cr
-#'   -- Germany total population \tab DEUTNP\cr
-#'   -- West Germany \tab DEUTFRG\cr
-#'   -- East Germany \tab DEUTGDR\cr
-#'   Greece \tab GRC\cr
-#'   Hungary \tab HUN\cr
-#'   Iceland \tab ISL\cr
-#'   Ireland \tab IRL\cr
-#'   Israel \tab ISR\cr
-#'   Italy \tab ITA\cr
-#'   Japan \tab JPN\cr
-#'   Latvia \tab LVA\cr
-#'   Lithuania \tab LTU\cr
-#'   Luxembourg \tab LUX\cr
-#'   Netherlands \tab NLD\cr
-#'   New Zealand	\cr
-#'   -- NZ total population \tab NZL_NP\cr
-#'   -- NZ Maori \tab NZL_MA\cr
-#'   -- NZ non-Maori \tab NZL_NM\cr
-#'   Norway \tab NOR\cr
-#'   Poland \tab POL\cr
-#'   Portugal \tab PRT\cr
-#'   Russia \tab RUS\cr
-#'   Slovakia \tab SVK\cr
-#'   Slovenia \tab SVN\cr
-#'   Spain \tab ESP\cr
-#'   Sweden \tab SWE\cr
-#'   Switzerland \tab CHE\cr
-#'   Taiwan \tab TWN\cr
-#'   United Kingdom\cr
-#'   -- UK Total Population \tab GBR_NP\cr
-#'   -- England & Wales Total Population \tab GBRTENW\cr
-#'   -- England & Wales Civilian Population \tab GBRCENW\cr
-#'   -- Scotland \tab GBR_SCO\cr
-#'   -- Northern Ireland \tab GBR_NIR\cr
-#'   U.S.A. \tab USA\cr
-#'   Ukraine \tab UKR\cr
-#' }
-#'
-#'
+#' and obtain a valid username and password. 
 #'
 #' @param country Directory abbreviation from the HMD. For instance, Australia =
-#'   "AUS". See below for other countries.
+#'   "AUS".
 #' @param username HMD username (case-sensitive)
 #' @param password HMD password (case-sensitive)
 #' @param label Character string giving name of country from which the data are
@@ -93,106 +34,78 @@
 #' @examples
 #' \dontrun{
 #' norway <- hmd.mx("NOR", username, password, "Norway")
-#' summary(norway)}
+#' summary(norway)
+#' }
 #' @keywords manip
 #' @name hmd
 #' @export
-hmd.mx <- function(country, username, password, label=country)
-{
-    path <- paste("https://former.mortality.org/hmd/", country, "/STATS/", "Mx_1x1.txt", sep = "")
-    userpwd <- paste(username, ":", password, sep = "")
-    txt <- RCurl::getURL(path, userpwd = userpwd)
-    con <- textConnection(txt)
-    mx <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
-    close(con)
-    if(inherits(mx, "try-error"))
-        stop("Connection error at www.mortality.org. Please check username, password and country label.")
+hmd.mx <- function(country, username, password, label = country) {
+  # Read raw MX and Exposure data
+  mx <- HMDHFDplus::readHMDweb(country, item = "Mx_1x1",
+    username = username, password = password, fixup = TRUE)
+  pop <- HMDHFDplus::readHMDweb(country, item = "Exposures_1x1",
+    username = username, password = password, fixup = TRUE)
 
-    path <- paste("https://former.mortality.org/hmd/", country, "/STATS/", "Exposures_1x1.txt", sep = "")
-    userpwd <- paste(username, ":", password, sep = "")
-    txt <- RCurl::getURL(path, userpwd = userpwd)
-    con <- textConnection(txt)
-    pop <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
-    close(con)
-    if(inherits(pop, "try-error"))
-        stop("Exposures file not found at www.mortality.org")
+  # Construct output
+  obj <- list(type = "mortality", label = label, lambda = 0)
+  obj$year <- sort(unique(mx[, "Year"]))
+  n <- length(obj$year)
+  m <- length(unique(mx[, "Age"]))
+  obj$age <- mx[seq(m), "Age"]
+  mnames <- names(mx)[-c(1:2, NCOL(mx))]
+  n.mort <- length(mnames)
+  obj$rate <- obj$pop <- list()
+  for (i in seq(n.mort)) {
+    obj$rate[[i]] <- matrix(mx[, i + 2], nrow = m, ncol = n)
+    obj$rate[[i]][obj$rate[[i]] < 0] <- NA
+    obj$pop[[i]] <- matrix(pop[, i + 2], nrow = m, ncol = n)
+    obj$pop[[i]][obj$pop[[i]] < 0] <- NA
+    dimnames(obj$rate[[i]]) <- dimnames(obj$pop[[i]]) <- list(obj$age, obj$year)
+  }
+  names(obj$pop) <- names(obj$rate) <- tolower(mnames)
 
-    obj <- list(type="mortality",label=label,lambda=0)
-
-    obj$year <- sort(unique(mx[, 1]))
-    #obj$year <- ts(obj$year, start=min(obj$year))
-    n <- length(obj$year)
-    m <- length(unique(mx[, 2]))
-    obj$age <- mx[1:m, 2]
-    mnames <- names(mx)[-c(1, 2)]
-    n.mort <- length(mnames)
-    obj$rate <- obj$pop <- list()
-    for (i in 1:n.mort)
-    {
-        obj$rate[[i]] <- matrix(mx[, i + 2], nrow = m, ncol = n)
-        obj$rate[[i]][obj$rate[[i]] < 0] <- NA
-        obj$pop[[i]] <- matrix(pop[, i + 2], nrow = m, ncol = n)
-        obj$pop[[i]][obj$pop[[i]] < 0] <- NA
-        dimnames(obj$rate[[i]]) <- dimnames(obj$pop[[i]]) <- list(obj$age, obj$year)
-    }
-    names(obj$pop) = names(obj$rate) <- tolower(mnames)
-
-    suppressWarnings(obj$age <- as.numeric(as.character(obj$age)))
-    if (is.na(obj$age[m]))
-        obj$age[m] <- 2 * obj$age[m - 1] - obj$age[m - 2]
-    return(structure(obj, class = "demogdata"))
+  return(structure(obj, class = "demogdata"))
 }
 
 #' @rdname hmd
 #' @export
-hmd.e0 <- function(country, username, password)
-{
-    path <- paste("https://former.mortality.org/hmd/", country, "/STATS/", "E0per.txt", sep = "")
-    userpwd <- paste(username, ":", password, sep = "")
-    txt <- RCurl::getURL(path, userpwd = userpwd)
-    con <- textConnection(txt)
-    lt <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."), TRUE)
-    close(con)
-    if (inherits(lt, "try-error"))
-        stop("Life expectancy file not found at www.mortality.org")
-	lt <- ts(lt[,-1],start=lt[1,1],frequency=1)
-    return(lt)
+hmd.e0 <- function(country, username, password) {
+  # Read raw e0 data
+  lt <- HMDHFDplus::readHMDweb(country, item = "E0per",
+    username = username, password = password, fixup = TRUE)
+  # Convert to a ts object
+  ts(lt[, -1], start = lt[1, 1], frequency = 1)
 }
 
 
 #' @rdname hmd
 #' @export
-hmd.pop <- function(country, username, password, label=country)
-{
-    path <- paste("https://former.mortality.org/hmd/", country, "/STATS/", "Population.txt", sep = "")
-    userpwd <- paste(username, ":", password, sep = "")
-    txt <- RCurl::getURL(path, userpwd = userpwd)
-    con <- textConnection(txt)
-    pop <- try(utils::read.table(con, skip = 2, header = TRUE, na.strings = "."),TRUE)
-    close(con)
-    if(inherits(pop, "try-error"))
-        stop("Population file not found at www.mortality.org")
+hmd.pop <- function(country, username, password, label = country) {
+  # Read raw data
+  pop <- HMDHFDplus::readHMDweb(country, item = "Population",
+    username = username, password = password, fixup = FALSE)
 
-    obj <- list(type="population",label=label,lambda=0)
+  # Only keep 1 January populations
+  pop <- pop[, !grepl("2$", colnames(pop))]
+  mnames <- names(pop)
+  mnames <- sub("1$", "", mnames)
+  names(pop) <- mnames
 
-    obj$year = sort(unique(pop[, 1]))
-    #obj$year <- ts(obj$year, start=min(obj$year))
-    n <- length(obj$year)
-    m <- length(unique(pop[, 2]))
-    obj$age <- pop[1:m, 2]
-    mnames <- names(pop)[-c(1, 2)]
-    n.pop <- length(mnames)
-    obj$pop <- list()
-    for (i in 1:n.pop)
-    {
-        obj$pop[[i]] <- matrix(pop[, i + 2], nrow = m, ncol = n)
-        obj$pop[[i]][obj$pop[[i]] < 0] <- NA
-        dimnames(obj$pop[[i]]) <- list(obj$age, obj$year)
-    }
-    names(obj$pop) <- tolower(mnames)
+  # Construct output
+  obj <- list(type = "population", label = label, lambda = 0)
+  obj$year <- sort(unique(pop[, "Year"]))
+  n <- length(obj$year)
+  m <- length(unique(pop[, "Age"]))
+  obj$age <- pop[seq(m), "Age"]
+  pop <- pop[, !(mnames %in% c("Year","Age","OpenInterval"))]
+  n.pop <- NCOL(pop)
+  obj$pop <- list()
+  for (i in seq(n.pop)) {
+    obj$pop[[i]] <- matrix(pop[, i], nrow = m, ncol = n)
+    obj$pop[[i]][obj$pop[[i]] < 0] <- NA
+    dimnames(obj$pop[[i]]) <- list(obj$age, obj$year)
+  }
+  names(obj$pop) <- tolower(colnames(pop))
 
-    suppressWarnings(obj$age <- as.numeric(as.character(obj$age)))
-    if (is.na(obj$age[m]))
-        obj$age[m] <- 2 * obj$age[m - 1] - obj$age[m - 2]
-    return(structure(obj, class = "demogdata"))
+  return(structure(obj, class = "demogdata"))
 }
